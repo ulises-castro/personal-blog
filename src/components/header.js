1a6891dbad8e0ed479edaf134c14f5efb0534396
Ulises Castro <ulisescastro2015@gmail.com>
2020-03-12 23:01:20 -0600

    Added header links

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,12 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -26,6 +32,12 @@ const Header = ({ siteTitle }) => (
         >
           {siteTitle}
         </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/tuturials/">Tuturials</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
       </h1>
     </div>
   </header>
