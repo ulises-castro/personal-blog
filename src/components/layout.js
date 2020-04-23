3a652bc19d896f184b87ea4c8fe06c2c5b17ca71
Ulises Castro <ulisescastro2015@gmail.com>
2020-04-23 16:20:39 -0500

    Changing main fonts and changing

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,48 +9,48 @@ const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
 
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
+  // if (location.pathname === rootPath) {
+  //   header = (
+  //     <h1
+  //       style={{
+  //         ...scale(1.5),
+  //         marginBottom: rhythm(1.5),
+  //         marginTop: 0,
+  //       }}
+  //     >
+  //       <Link
+  //         style={{
+  //           boxShadow: `none`,
+  //           textDecoration: `none`,
+  //           color: `inherit`,
+  //         }}
+  //         to={`/`}
+  //       >
+  //         {title} <GiSpartanHelmet />
+  //       </Link>
+  //     </h1>
+  //   )
+  // } else {
+  // }
+  header = (
+    <h3
+      style={{
+        fontFamily: `Raleway, Montserrat, sans-serif`,
+        marginTop: 0,
+      }}
+    >
+      <Link
         style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
+          boxShadow: `none`,
+          textDecoration: `none`,
+          color: `inherit`,
         }}
+        to={`/`}
       >
-        <Link
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title} <GiSpartanHelmet />
-        </Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title} <GiSpartanHelmet />
-        </Link>
-      </h3>
-    )
-  }
+        {title} <GiSpartanHelmet />
+      </Link>
+    </h3>
+  )
   return (
     <div
       style={{
