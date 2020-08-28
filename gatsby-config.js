daa91e292295401bacc9c8a5dbf15c49d3cfe463
Ulises Castro <ulisescastro2015@gmail.com>
2020-08-28 17:19:45 -0500

    Changed blog theme for a good one

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,115 +1,61 @@
 module.exports = {
   siteMetadata: {
     title: `Warrior Code`,
-    author: {
-      name: `Ulises Castro`,
-      summary: `who is a Software Engineer focuses itself on creating amazing web and mobile applications.`,
-    },
-    description: `Easy way to solving complex problems.`,
+    name: `WarriorCode`,
     siteUrl: `https://warriorcode.net`,
-    social: {
-      twitter: `ulises-castro`,
+    description: `This is my description that will be used in the meta tags and important for search results`,
+    hero: {
+      heading: `Welcome to Warrior Code, the simplest way to start learn how to code.`,
+      maxWidth: 652,
     },
-    menuLinks: [
+    social: [
       {
-        name: `Blog`,
-        url: `/`,
+        name: `twitter`,
+        url: `https://twitter.com/narative`,
       },
       {
-        name: `Tutorials`,
-        url: `/tutorials`,
+        name: `github`,
+        url: `https://github.com/ulises-castro`,
       },
       {
-        name: `Notes`,
-        url: `/notes`,
+        name: `instagram`,
+        url: `https://instagram.com/narative.co`,
       },
       {
-        name: `Contact`,
-        url: `/contact`,
-      },
-      {
-        name: `About me`,
-        url: `https://ulises-castro.github.io/`,
+        name: `linkedin`,
+        url: `https://www.linkedin.com/company/narative/`,
       },
     ],
   },
   plugins: [
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-plugin-sass`,
+      resolve: "@narative/gatsby-theme-novela",
       options: {
-        postCssPlugins: [
-          require("tailwindcss"),
-          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
-        ],
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        authorsPage: true,
+        sources: {
+          local: true,
+          // contentful: true,
+        },
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [{
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Novela by Narative`,
+        short_name: `Novela`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`,
       },
     },
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {},
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
-}
+};
