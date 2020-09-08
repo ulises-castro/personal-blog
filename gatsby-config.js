b8dc55219cd2edfa2f2b309ba1bc7741d0fbbddc
Ulises Castro <ulisescastro2015@gmail.com>
2020-09-08 11:38:36 -0500

    Restore to new blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,11 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
 module.exports = {
   siteMetadata: {
-    title: `Warrior Code`,
-    name: `WarriorCode`,
-    siteUrl: `https://warriorcode.net`,
+    title: `Novela by Narative`,
+    name: `Narative`,
+    siteUrl: `https://novela.narative.co`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Warrior Code, the simplest way to start learn how to code.`,
+      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
       maxWidth: 652,
     },
     social: [
@@ -19,7 +15,19 @@ module.exports = {
       },
       {
         name: `github`,
-        url: `https://github.com/ulises-castro`,
+        url: `https://github.com/narative`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/narative.co`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/company/narative/`,
+      },
+      {
+        name: `dribbble`,
+        url: `https://dribbble.com/narativestudio`,
       },
     ],
   },
@@ -30,10 +38,9 @@ module.exports = {
         contentPosts: "content/posts",
         contentAuthors: "content/authors",
         basePath: "/",
-        mailchimp: true,
         authorsPage: true,
         sources: {
-          // local: true,
+          local: true,
           // contentful: true,
         },
       },
@@ -52,12 +59,7 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-netlify-cms`,
-      options: {},
-    },
-    {
-      resolve: "gatsby-plugin-mailchimp",
       options: {
-        endpoint: process.env.MAILCHIMP_ENDPOINT, // add your MC list endpoint here; see plugin repo for instructions
       },
     },
   ],
