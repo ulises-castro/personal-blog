37eea7ebca93fb2ab025505a0d6d335c028b6d4f
Ulises Castro <ulisescastro2015@gmail.com>
2020-09-12 23:12:58 -0500

    Replace logo

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import svgLogo from "./wc-logo.png";
+
+/**
+ * Paste in your SVG logo and return it from this component.
+ * Make sure you have a height set for your logo.
+ * It is recommended to keep the height within 25-35px.
+ * Logo comes with a property value called `fill`. `fill` is useful
+ * when you want to change your logo depending on the theme you are on.
+ */
+
+export default function Logo() {
+  return <img height="110" src={svgLogo} />;
+}
